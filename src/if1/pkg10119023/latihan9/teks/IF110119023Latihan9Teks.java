d3f09a4fe70cd54cd8bcd81a13c1bd30ab00cce2
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 20:10:40 +0700

    Latihan 9 - Teks

---
+++ src/if1/pkg10119023/latihan9/teks/IF110119023Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan9.teks;
+
+/**
+ *
+ * @author   
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : latihan membuat teks(char, spasi(tab), baris baru
+ */
+public class IF110119023Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: "+ tab +"berisi "+c);
+    }
+    
+}
